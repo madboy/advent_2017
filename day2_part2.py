1964d5e45d1f6b672a3e6dd5f09677493c621edd
Kristofer Lindberg <k.lindberg@gmail.com>
2017-12-28 08:56:50 +0100

    no need to sort

--- day2_part2.py
+++ day2_part2.py
@@ -1,21 +1,17 @@
 import fileinput
 
-def checksum(row):
-    numbers = row.split("\t")
-    numbers = list(map(int, numbers))
-    numbers.sort()
-    for i, number in enumerate(numbers):
-        n = int(number)
-        for nn in numbers[i+1:]:
-            if int(nn)%n == 0:
-                return int(nn)/n
-    return 0
 
-total = 0
+def checksum(row):
+    numbers = list(map(int, row.split("\t")))
+    for n in numbers:
+        for nn in numbers:
+            if n> nn and n%nn == 0:
+                return n/nn
+
+
+rows = []
 for line in fileinput.input():
     row = line.strip()
-    cs = checksum(row)
-    assert cs != 0, row
-    total += cs
+    rows.append(row)
 
-print(total)
+print(sum(map(checksum, rows)))
