3c5aa845c75d864517eca668fe601f54e4186f0e
Kristofer Lindberg <k.lindberg@gmail.com>
2017-12-28 08:59:54 +0100

    doc

--- day2_part2.py
+++ day2_part2.py
@@ -1,3 +1,10 @@
+"""
+It sounds like the goal is to find the only two numbers in each row 
+where one evenly divides the other - that is, where the result of the 
+division operation is a whole number.
+
+They would like you to find those numbers on each line, divide them, and add up each line's result.
+"""
 import fileinput
 
 
